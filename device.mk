9569b9e987a94af9a446f5de31307e69baa47494
NightDistortion <124385894+NightDistortion@users.noreply.github.com>
2023-02-03 21:18:32 +0300

    begonia: include firmware and prebuilt recovery

--- device.mk
+++ device.mk
@@ -134,6 +134,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm-service.clearkey
 
+# Prebuilt firmware & recovery 
+$(call inherit-product, vendor/redmi/begonia-firmware/begonia-firmware.mk)
+
 # FM Radio
 PRODUCT_PACKAGES += \
     MtkFMRadio
